91b0868e5d3cfeab69339c4275870015425728eb
p-stryuk-goodt <114069289+p-stryuk-goodt@users.noreply.github.com>
2023-03-29 11:54:09 +0300

    rewrite bot upgraded to 3.5

--- rewrite_bot_3.5.py
+++ rewrite_bot_3.5.py
@@ -1,13 +1,17 @@
 import telebot
 import openai
 import time
+from googletrans import Translator
 
 bot = telebot.TeleBot("6282182565:AAFFt_rjHhh9gf1bwr8LQAR7DmH81tWf24A")
-openai.api_key = "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"
+openai.api_key = "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"
 model = "gpt-3.5-turbo"
 stop_symbols = "###"
-
+default_mes = [{"role": "system", "content": "You're a bot who paraphrases the text. You just take the text and send a "
+                                             "new version of it, with no comments."}]
+translator = Translator()
 users = {}
+this_time = time.time()
 
 
 def _get_user(id):
@@ -18,24 +22,33 @@ def _get_user(id):
 
 def _process_rq(user_id, rq):
     user = _get_user(user_id)
-    # if last prompt time > 10 minutes ago - drop context
     if time.time() - user['last_prompt_time'] > 600:
         user['last_prompt_time'] = 0
-        user['messages'] = []
+        user['messages'] = default_mes
 
     if rq and len(rq) > 0 and len(rq) < 3000:
+
+        rq = translator.translate(rq, dest='en', src='ru').text
+
         print(f">>> ({user_id}) {rq}")
         message = {"role": "user", "content": rq}
         user['messages'].append(message)
         user['messages'] = user['messages'][-6:]
         print("Sending to OpenAI: " + user['messages'][-1]["content"])
         full_ans = openai.ChatCompletion.create(
-          model="gpt-3.5-turbo",
-          messages=user['messages']
+            model="gpt-3.5-turbo",
+            messages=user['messages']
         ).choices[0]
-        ans = full_ans.message["content"]
+        eng_ans = full_ans.message["content"]
+        if "->" in eng_ans:
+            eng_ans = eng_ans.split("->")[0].strip()
+        ans = translator.translate(eng_ans, dest='ru', src='en').text
+
         ans_to_dict = {"role": "assistant", "content": ans}
-        user['messages'].append(ans_to_dict)
+
+        # user['messages'].append(ans_to_dict)
+        user['messages'] = default_mes
+
         user['messages'] = user['messages'][-6:]
         print(f"<<< ({user_id}) {ans}")
         user['last_prompt_time'] = time.time()
@@ -58,6 +71,8 @@ def send_welcome(message):
 def echo_all(message):
     user_id = message.from_user.id
     rq = message.text
+    if time.time() - this_time < 2:
+        return 0
     if rq.lower() in ('пошёл нахуй', 'забудь', 'обама', 'пошел нахуй', 'иди нахуй', 'ебало завали', 'завали ебало'):
         user = _get_user(user_id)
         user['last_prompt_time'] = 0
